49fa618255825be54bfb4801c5d5e59f308640ab
Jeff Steed <steed.jeff@gmail.com>
2016-01-22 22:47:49 -0600

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # returns the full title per page
+  def full_title(page_title = '')
+    base_title = "Decksmith"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  root 'static_pages#index'
+  root 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
 
