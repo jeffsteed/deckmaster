c0047e11aae0cd66250837a0c39f3ee36fc84fa1
Jeff Steed <steed.jeff@gmail.com>
2016-01-23 14:20:59 -0600

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,23 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # successful registration
+      flash[:success] = "Welcome to Decksmith!"
+      redirect_to @user
+    else
+      # unsuccessful registration
+      render "new"
+    end
   end
 
   def edit
@@ -19,4 +30,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
